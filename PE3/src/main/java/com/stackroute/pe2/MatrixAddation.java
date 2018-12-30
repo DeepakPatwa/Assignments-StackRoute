a635449009953a4dc7e7bf504e33e828860e7409
Deepak Patwa <deepakgkp192@gmail.com>
2018-12-31 02:00:40 +0530

    final commit

--- PE3/src/main/java/com/stackroute/pe2/MatrixAddation.java
+++ PE3/src/main/java/com/stackroute/pe2/MatrixAddation.java
@@ -26,35 +26,35 @@ public class MatrixAddation extends Matrix{
         return matrixSum;
     }
 
-    public static void main(String [] args)
-    {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter no of rows and colomns");
-
-        MatrixAddation ma = new MatrixAddation();
-        ma.rows=in.nextInt();
-        ma.colomns=in.nextInt();
-        //AddMatrix(rows, colomns);
-        int[][] matrix1 = new int[ma.rows][ma.colomns];;//matrix 1
-        int[][] matrix2 = new int[ma.rows][ma.colomns];//matrix2
-        int[][] matrixSum = new int[ma.rows][ma.colomns];//sum of matrix1 and matrix2
-
-        //entering values in first matrix
-        for(int i=0;i<ma.rows;i++)
-            for(int j=0;j<ma.colomns;j++)
-                matrix1[i][j]=in.nextInt();
-
-         //entering values in second matrix
-        for(int i=0;i<ma.rows;i++)
-            for(int j=0;j<ma.colomns;j++)
-                matrix2[i][j]=in.nextInt();
-
-//         // adding both of tthe matrix to matrixSum
+//    public static void main(String [] args)
+//    {
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Enter no of rows and colomns");
+//
+//        MatrixAddation ma = new MatrixAddation();
+//        ma.rows=in.nextInt();
+//        ma.colomns=in.nextInt();
+//        //AddMatrix(rows, colomns);
+//        int[][] matrix1 = new int[ma.rows][ma.colomns];;//matrix 1
+//        int[][] matrix2 = new int[ma.rows][ma.colomns];//matrix2
+//        int[][] matrixSum = new int[ma.rows][ma.colomns];//sum of matrix1 and matrix2
+//
+//        //entering values in first matrix
 //        for(int i=0;i<ma.rows;i++)
 //            for(int j=0;j<ma.colomns;j++)
-//                matrixSum[i][j]=matrix1[i][j] + matrix2[i][j];
-        matrixSum=AddMatrix(matrix1,matrix2,ma.rows,ma.colomns);
-
-        ma.PrintMatrix(ma.rows, ma.colomns, matrixSum);
-    }
+//                matrix1[i][j]=in.nextInt();
+//
+//         //entering values in second matrix
+//        for(int i=0;i<ma.rows;i++)
+//            for(int j=0;j<ma.colomns;j++)
+//                matrix2[i][j]=in.nextInt();
+//
+////         // adding both of tthe matrix to matrixSum
+////        for(int i=0;i<ma.rows;i++)
+////            for(int j=0;j<ma.colomns;j++)
+////                matrixSum[i][j]=matrix1[i][j] + matrix2[i][j];
+//        matrixSum=AddMatrix(matrix1,matrix2,ma.rows,ma.colomns);
+//
+//        ma.PrintMatrix(ma.rows, ma.colomns, matrixSum);
+//    }
 }
